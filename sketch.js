18361e9817b0a8e57091b35ca6d0026bbba42fe8
AnyySann <86086338+anyy520@users.noreply.github.com>
2023-12-06 10:55:47 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Body = Matter.Body;
 // 3
 var btn2;
 //5  Vamos criar uma variável de ângulo e defini-la com um valor aleatório de 60
-var angle=60;
+
 
 
 function setup() {
@@ -29,8 +29,10 @@ function setup() {
   btn2.position(350,30);
   btn2.size(50,50);
   btn2.mouseClicked(vForce);
+  
 // 4. Verifique a instrução abaixo no arquivo sketch.js e altere os valores do retângulo.
-  ground1 = Bodies.rectangle(100,300,100,20,ground_options);
+  
+  ground1 = Bodies.rectangle(100,400,400,20,ground_options);
   World.add(world,ground1);
 
   ball = Bodies.circle(100,10,20,ball_options);
@@ -51,24 +53,26 @@ function draw()
   Engine.update(engine);
   
   // 6 Adicione o código para girar o objeto dentro da função draw(). 
-  Matter.Body.rotate(ground1,angle);
+  
+ 
   //8  Gire o solo adicionando as funções push() e pop() dentro da função draw():
 
-  push();
+  
   translate(ground1.position.x,ground1.position.y);
-  rotate(angle);
+  
+  
   rect(0,0,100,20);
-  pop();
+ 
   
   // 7 . Aumente o ângulo da rotação em 0.1 graus dentro da função draw.
-  angle +=0.1;
+ 
 
  
 // Exibe a bola passando sua posição na função elipse.
   ellipse(ball.position.x,ball.position.y,20);
   rect(ground.position.x,ground.position.y,650,20);
  
-//console.log(ground.position.y);
+console.log(ground.position.y);
 
   
   
